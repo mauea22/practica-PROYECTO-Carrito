76dd91b41e7e42e59d40e4065ccea98f9741c8ae
mauea22 <mauroallena@gmail.com>
2022-04-23 19:47:11 -0300

    funcion cargar datos y leer datos de cursos

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const listaCursos = document.querySelector('#lista-cursos');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
 
-//función para cargar los eventListeners
+//función para registrar todos los eventListeners
 cargarEventListeners();
 function cargarEventListeners() {
     // Cuando agregas un curso presionando "Agregar al Carrito"
@@ -22,7 +22,39 @@ function cargarEventListeners() {
 
 
 //* FUNCIONES
+//esta función se ejecuta después de escuchar el evento click (cargarEventListener)
 
-function agregarCursos() {
-    console.log("agregando al carrito");
+function agregarCursos(e) {
+    //prevent evita ese salto hacia el href que tiene el elemento <a></a>
+    e.preventDefault();
+    //e.target se refiere elemento clickado (en este caso el tag <a></a> dentro del <div id="lista-cursos"></div>)
+    //si ese elemento contiene dentro de sus clases (agregar-carrito) ejecuto el if
+    if (e.target.classList.contains('agregar-carrito')){
+        //estoy subiendo al padre del elemento para tener acceso a los datos del curso 
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        //llamo a la función que lee los datos y le paso la variable que contiene el elemento raíz 
+        leerDatosCurso(cursoSeleccionado);
+    }
+    
 }
+
+
+// lee el contenido HTML al que le dimos click y extrae la información del curso
+function leerDatosCurso(curso){
+    
+    //crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        //obtengo la img
+        imagen: curso.querySelector('img').src,
+        //obtengo el yiyulo del curso 
+        titulo: curso.querySelector('h4').textContent,
+        //obtengo el precio con rebaja que es el texto dentro del span con la clase .precio
+        precio: curso.querySelector('.precio span').textContent,
+        //cada curso tiene un id para acceder obtengo el atributo (data-id) del elemento (<a></a>)
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    console.log(infoCurso);
+}
+
